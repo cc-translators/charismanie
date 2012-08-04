109a9bc70df240015315b43044f64207055a4187
Raphaël Pinson <raphink@gmail.com>
2012-08-04 22:26:11 +0200

    Fix accents and ligatures in ebooks

--- cleanuphtml.sh
+++ cleanuphtml.sh
@@ -19,16 +19,39 @@ echo ".fxlri-t-1x-x-120 {font-style:italic;}" >> $CSS
 echo "h2 {page-break-before: always}" >> $CSS
 
 
-# Force page breaks in intro
-echo ".pagebreak {page-break-before: always}" >> $CSS
-## Before thanks
-sed -i 's@\(<!--l. 2--><p class="indent\)" >$@\1 pagebreak" >@' $HTML
-## Before ToC
-#sed -i 's@\(<!--l. 17--><p class="indent\)" >$@\1 pagebreak" >@' $HTML
+## Accents
+for class in "fxlrc-t1-" "fxlrc-t1-x-x-120" "fxlbc-t1-x-x-248" "fxlbc-t1-x-x-144"; do
+   while read c r; do
+      sed -i "s@\(\"$class\">\)$c\([^<]*\)<@\1<span class=\"small-caps\">$r\2</span><@" $HTML
+   done <<<"é É
+è È
+ê Ê
+ï Ï
+î Î
+ﬁ FI
+ﬀ  FF
+ﬂ FL
+ﬃ   FFI
+â Â
+à À
+ù Ù
+û Û
+ô Ô"
+done
+
+## Ligatures
+while read c r; do
+   sed -i "s@\"small-caps\">$c<@\"small-caps\">$r<@" $HTML
+done <<<"ﬁ FI
+ﬀ  FF
+ﬂ FL
+ﬃ   FFI"
+
 
 # Lettrines
 echo ".lettrine:first-letter{font-size:6em;margin-right:3px;display:inline;line-height:0.5em;color:gray;float:left;}" >> $CSS
 sed -i '/<h2/,/<h3/ { s@\(<p class="noindent\)" >@\1 lettrine" >@  }' $HTML
+
 ## And restore indentation in the beginning of sections
 sed -i '/<h2/,/<\/html/ { s@"noindent"@"indent"@ }' $HTML
 
