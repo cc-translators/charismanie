8df62c9c944951cde769e7b7c7eb9c205ad4aa1b
Raphaël Pinson <raphink@gmail.com>
2012-08-05 22:44:29 +0200

    Setup indentation of all paragraphs in charismanie.4ht

--- cleanuphtml.sh
+++ cleanuphtml.sh
@@ -6,6 +6,3 @@ CSS="$(basename $HTML .html).css"
 # Remove space before nbsp
 sed -i "s@ *&nbsp; *@\&nbsp;@g" $HTML
 
-## And restore indentation in the beginning of sections
-sed -i '/<h2/,/<\/html/ { s@"noindent"@"indent"@ }' $HTML
-
