5c72f0ffa8870e91dab8916219ada113615932b7
Raphaël Pinson <raphink@gmail.com>
2012-08-05 18:34:59 +0200

    Move TeX4HT settings to .4ht files

--- cleanuphtml.sh
+++ cleanuphtml.sh
@@ -6,35 +6,6 @@ CSS="$(basename $HTML .html).css"
 # Remove space before nbsp
 sed -i "s@ *&nbsp; *@\&nbsp;@g" $HTML
 
-## Accents
-for class in "fxlrc-t1-" "fxlrc-t1-x-x-120" "fxlbc-t1-x-x-248" "fxlbc-t1-x-x-144" "cmcsc-10x-x-120"; do
-   while read c r; do
-      sed -i "s@\(\"$class\">\)$c\([^<]*\)<@\1<span class=\"small-caps\">$r\2</span><@" $HTML
-   done <<<"é é
-è è
-ê ê
-ï ï
-î î
-ﬁ ﬁ
-ﬀ ﬀ
-ﬂ ﬂ
-ﬃ  ﬃ
-â â
-à à
-ù ù
-û û
-ô ô"
-done
-
-
-# Lettrines
-echo ".lettrine:first-letter{font-size:6em;margin-right:3px;display:inline;line-height:0.5em;color:gray;float:left;}" >> $CSS
-sed -i '/<h2/,/<h3/ { s@\(<p class="noindent\)" >@\1 lettrine" >@  }' $HTML
-
 ## And restore indentation in the beginning of sections
 sed -i '/<h2/,/<\/html/ { s@"noindent"@"indent"@ }' $HTML
-
-
-# TODO: try to fix the ToC
-
 
